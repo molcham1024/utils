73426bb0043009cef822b127e7bf31fbb26d6f4d
molcham1024 <sakuma121024@gmail.com>
2025-09-07 01:35:37 +0900

    feat: アクセスログをCSVに変換するシェルスクリプトを追加

---
+++ convert_access_log_to_csv/nginx_access_log_to_csv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# usage
+# ./nginx_access_log_to_csv.sh input_file output_file
+
+if [ -z "$2" ] || [ -n "$3" ]; then
+    echo 'Error: Enter 2 arguments "input_file","output_file".'
+    echo 'Usage: ./nginx_access_log_to_csv.sh {input_file} {output_file}'
+    exit
+fi
+
+INPUT_FILE="$1"
+OUTPUT_FILE="$2"
+
+if [ ! -e "$INPUT_FILE" ]; then
+    echo "Error: \"$INPUT_FILE\" do not exists."
+    exit
+fi
+
+if [ -e "$OUTPUT_FILE" ]; then
+    read -p "The file named \"$OUTPUT_FILE\" already exists. Do you want to overwrite it? [y/n]: " yn
+
+    case "$yn" in
+        [Nn])
+            echo 'Abort.'
+            exit
+            ;;
+    esac
+
+fi
+
+echo '"IP/Host","remote_log","remote_user","date","method","request (without parameters)","parameters","http_version","status","body_bytes_sent","referer","user_agent","x_forwarded_for"' > $OUTPUT_FILE
+
+perl -pe 's/^(.*?) (.*?) (.*?) \[(.*?)\] "(.*?) (.*?)(\?.*?)? (.*?)" (.*?) (.*?) "(.*?)" "(.*?)" "(.*?)"$/"$1","$2","$3","$4","$5","$6","$7","$8","$9","$10","$11","$12","$13"/' $INPUT_FILE >> $OUTPUT_FILE
